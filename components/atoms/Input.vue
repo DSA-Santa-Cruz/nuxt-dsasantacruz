d31c6f65ec38b83ddb12fae49e42621c41b64cbe
Neil Ruud <neil@ruudmedia.com>
2022-09-01 18:23:43 -0700

    fix </script> typo

--- components/atoms/Input.vue
+++ components/atoms/Input.vue
@@ -17,7 +17,6 @@ const emit = defineEmits(["update:modelValue"]);
 const updateValue = event => {
   emit("update:modelValue", event.target.value);
 };
-</script>
 export default {
   props: {
     label: {
