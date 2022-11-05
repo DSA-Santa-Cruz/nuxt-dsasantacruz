990ccf3b4ff6356a6532955c0cee8cb225aa4811
Neil Ruud <neil@ruudmedia.com>
2022-11-04 17:01:03 -0700

    various improvements

---
+++ components/atoms/Icon.vue
@@ -0,0 +1,22 @@
+<template>
+  <component v-if="icon" :is="icon" class="transition duration-100" />
+</template>
+
+<script setup>
+import { defineAsyncComponent } from "vue";
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  variant: {
+    type: String,
+    default: "solid",
+  },
+});
+const icon = props.name
+  ? defineAsyncComponent(() =>
+      import(`../../svg/${props.variant}/${props.name}.svg`),
+    )
+  : null;
+</script>
